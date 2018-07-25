b6e328a622b4de0ae998875fae1cf458147fe775
0xFireball <0xFireball@outlook.com>
2018-07-25 09:52:13 -0700

    equality overrides for position

--- src/SquareEmpires/WireSpire/Types/Position.cs
+++ src/SquareEmpires/WireSpire/Types/Position.cs
@@ -2,8 +2,8 @@
 
 namespace WireSpire.Types {
     public struct Position {
-        public int x;
-        public int y;
+        public readonly int x;
+        public readonly int y;
 
         public Position(int x, int y) {
             this.x = x;
@@ -21,5 +21,24 @@ namespace WireSpire.Types {
         public static int tileDistance(Position p1, Position p2) {
             return Math.Abs(p1.x - p2.x) + Math.Abs(p1.y - p2.y);
         }
+
+        public override bool Equals(object obj) {
+            if (obj == null || GetType() != obj.GetType())
+                return false;
+
+            return equalTo((Position) obj);
+        }
+
+        public override int GetHashCode() {
+            unchecked // Overflow is fine, just wrap
+            {
+                var hash = 17;
+                hash = hash * 23 + x.GetHashCode();
+                hash = hash * 23 + y.GetHashCode();
+                return hash;
+            }
+        }
+
+        public bool equalTo(Position p) => (x == p.x) && (y == p.y);
     }
 }
